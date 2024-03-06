29d5f150bdd045fc295aa5389882205f1f452711
Pedro Baruc <devpedrobaruc@gmail.com>
2024-03-06 00:27:48 -0300

    Refactor main.dart and add HomeScreen

--- netflix_main_interface_clone/lib/main.dart
+++ netflix_main_interface_clone/lib/main.dart
@@ -1,100 +1,21 @@
 import 'package:flutter/material.dart';
+import 'package:netflix_main_interface_clone/screens/home.dart';
 
 void main() {
-  runApp(const NetflixInterface());
+  runApp(const Navigator());
 }
 
-class NetflixInterface extends StatelessWidget {
-  const NetflixInterface({super.key});
-
-  final String _logoUrl =
-      "https://i.pinimg.com/736x/e9/88/40/e9884007598e2e329d53bb448ede4084.jpg";
-  final String _iconSearch =
-      "https://i.pinimg.com/736x/e9/88/40/e9884007598e2e329d53bb448ede4084.jpg";
+class Navigator extends StatelessWidget {
+  const Navigator({super.key});
 
   @override
   Widget build(BuildContext context) {
-    final width = MediaQuery.of(context).size.width;
-
     return MaterialApp(
       theme: ThemeData(
         primaryColor: Colors.black,
         scaffoldBackgroundColor: Colors.black,
       ),
-      home: ListView(
-        children: <Widget>[
-          Container(
-            padding: const EdgeInsets.only(right: 20, left: 20),
-            child: Row(
-              children: <Widget>[
-                Image.network(
-                  _logoUrl,
-                  width: 40,
-                  fit: BoxFit.contain,
-                ),
-                const Spacer(
-                  flex: 1,
-                ),
-                Image.network(
-                  _iconSearch,
-                  width: 40,
-                  fit: BoxFit.contain,
-                )
-              ],
-            ),
-          ),
-          _section('Séries', width),
-          _section('Séries', width),
-          _section('Séries', width),
-          _section('Séries', width),
-          _section('Séries', width),
-          _section('Séries', width),
-          _section('Séries', width),
-          _section('Séries', width),
-          _section('Séries', width),
-        ],
-      ),
-    );
-  }
-
-  Widget _section(String title, double width) {
-    final cardWidth = width ~/ 3.5;
-
-    return Container(
-      padding: const EdgeInsets.only(top: 10),
-      child: Column(
-        children: <Widget>[
-          Row(
-            children: <Widget>[
-              Text(
-                title,
-                style: const TextStyle(
-                  color: Colors.white,
-                  fontSize: 20,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-            ],
-          ),
-          SingleChildScrollView(
-            scrollDirection: Axis.horizontal,
-            padding: const EdgeInsets.only(top: 10, bottom: 10),
-            child: Row(
-              children: List.generate(
-                20,
-                (index) => Container(
-                  margin: const EdgeInsets.only(right: 10),
-                  child: Image.network(
-                    _logoUrl,
-                    width: cardWidth.toDouble(),
-                    fit: BoxFit.contain,
-                  ),
-                ),
-              ).toList(),
-            ),
-          ),
-        ],
-      ),
+      home: const HomeScreen(),
     );
   }
 }
---
+++ netflix_main_interface_clone/lib/screens/home.dart
@@ -0,0 +1,128 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  final String _logoUrl =
+      "https://i.pinimg.com/736x/e9/88/40/e9884007598e2e329d53bb448ede4084.jpg";
+  final String _iconSearch =
+      "https://i.pinimg.com/736x/e9/88/40/e9884007598e2e329d53bb448ede4084.jpg";
+
+  @override
+  Widget build(BuildContext context) {
+    final media = MediaQuery.of(context);
+
+    return Container(
+        padding: EdgeInsets.only(
+            top: media.viewPadding.top, bottom: media.viewPadding.bottom),
+        child: Stack(
+          children: <Widget>[
+            ListView(
+              padding: const EdgeInsets.only(top: 50),
+              children: <Widget>[
+                _section(
+                  title: 'Séries',
+                  width: media.size.width,
+                ),
+                _section(
+                  title: 'Séries',
+                  width: media.size.width,
+                ),
+                _section(
+                  title: 'Séries',
+                  width: media.size.width,
+                ),
+                _section(
+                  title: 'Séries',
+                  width: media.size.width,
+                ),
+                _section(
+                  title: 'Séries',
+                  width: media.size.width,
+                ),
+                _section(
+                  title: 'Séries',
+                  width: media.size.width,
+                ),
+              ],
+            ),
+            Positioned(
+              child: Container(
+                padding: const EdgeInsets.only(right: 20, left: 20),
+                height: 50,
+                color: Theme.of(context).primaryColor,
+                child: Row(
+                  children: <Widget>[
+                    Center(
+                      child: Image.network(
+                        _logoUrl,
+                        width: 40,
+                        fit: BoxFit.contain,
+                      ),
+                    ),
+                    const Spacer(
+                      flex: 1,
+                    ),
+                    Center(
+                      child: Image.network(
+                        _iconSearch,
+                        width: 40,
+                        fit: BoxFit.contain,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ));
+  }
+
+  Widget _section({String title = '', double width = 10}) {
+    final cardWidth = width ~/ 3.5;
+
+    return Container(
+      padding: const EdgeInsets.only(top: 10),
+      child: Column(
+        children: <Widget>[
+          Container(
+            padding: const EdgeInsets.only(right: 20, left: 20),
+            child: Row(
+              children: <Widget>[
+                Text(
+                  title,
+                  style: const TextStyle(
+                    color: Colors.white,
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ],
+            ),
+          ),
+          SingleChildScrollView(
+            scrollDirection: Axis.horizontal,
+            padding:
+                const EdgeInsets.only(top: 10, bottom: 10, left: 10, right: 10),
+            child: Row(
+              children: List.generate(
+                20,
+                (index) => Container(
+                  margin: const EdgeInsets.only(right: 10),
+                  child: ClipRRect(
+                    borderRadius: BorderRadius.circular(6),
+                    child: Image.network(
+                      _logoUrl,
+                      width: cardWidth.toDouble(),
+                      fit: BoxFit.contain,
+                    ),
+                  ),
+                ),
+              ).toList(),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
